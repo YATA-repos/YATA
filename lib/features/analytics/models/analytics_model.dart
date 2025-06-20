946e19edc305fe26ac9b243741100583b53a5b13
penne <penne0505pp@gmail.com>
2025-06-20 23:40:45 +0900

    feat: 日別集計モデルを追加
    analytics_mode.dartに、日別集計に使用するドメインモデルを作成

--- lib/features/analytics/models/analytics_model.dart
+++ lib/features/analytics/models/analytics_model.dart
@@ -1,1 +1,35 @@
+import "../../../core/base/base.dart";
 
+/// 日別集計
+class DailySummary extends BaseModel {
+  /// コンストラクタ
+  DailySummary({
+    required this.summaryDate,
+    required this.completedOrders,
+    required this.totalRevenue,
+    required this.mostPopularItemCount,
+    this.averagePrepTimeMinutes,
+    this.mostPopularItemId,
+  });
+
+  /// 集計日
+  DateTime summaryDate;
+
+  /// 総注文数
+  int completedOrders;
+
+  /// 総売上
+  int totalRevenue;
+
+  /// 最も人気のある商品の注文数
+  int mostPopularItemCount;
+
+  /// 平均調理時間（分）
+  int? averagePrepTimeMinutes;
+
+  /// 最も人気のある商品のID
+  String? mostPopularItemId;
+
+  @override
+  String get tableName => "daily_summary";
+}
