6b5ff4422dd94439fbc6c940229719e11bd2c7f6
penne <penne0505pp@gmail.com>
2025-07-19 19:21:11 +0900

    refactor: simplify app bar to basic title-only layout
    - Remove complex navigation tab bar and action button components
    - Replace with simple app title with coffee icon
    - Apply new spacing constants from updated theme system
    - Streamline component structure for better maintainability

--- lib/shared/widgets/custom_app_bar.dart
+++ lib/shared/widgets/custom_app_bar.dart
@@ -1,111 +1,33 @@
 import "package:flutter/material.dart";
 import "package:lucide_icons/lucide_icons.dart";
 
-import "../../core/constants/constants.dart";
+import "../../core/core.dart";
 import "../themes/themes.dart";
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({required this.title, super.key});
-  final String title;
+  const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) => AppBar(
-    title: Apptitle(title: title),
-    actions: <Widget>[
-      NavigationTabBar(
-        items: <NavigationTabItem>[
-          NavigationTabItem(icon: LucideIcons.home, label: AppStrings.buttonHome),
-          NavigationTabItem(icon: LucideIcons.list, label: AppStrings.buttonOrderHistory),
-          NavigationTabItem(icon: LucideIcons.barChart, label: AppStrings.buttonAnalytics),
-        ],
-      ),
-      ActionButtonRow(
-        children: <Widget>[
-          ActionButton(
-            icon: LucideIcons.settings,
-            onPressed: () {
-              // Settings action
-            },
-          ),
-        ],
-      ),
-    ],
-  );
+      title: AppTitle(),
+    );
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(AppLayout.appBarHeight);
 }
 
-class Apptitle extends StatelessWidget {
-  const Apptitle({required this.title, this.icon = LucideIcons.coffee, super.key});
-
-  final String title;
-  final IconData icon;
+class AppTitle extends StatelessWidget {
+  const AppTitle({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) => Row(
-    children: <Widget>[
-      Icon(icon, size: 24),
-      const SizedBox(width: 8),
-      Text(title, style: AppTextStyles.textXl, overflow: TextOverflow.ellipsis, maxLines: 1),
-    ],
-  );
+      children: <Widget>[
+        Icon(LucideIcons.coffee, size: AppLayout.iconSize),
+        SizedBox(width: AppLayout.spacingXs),
+        const Text(AppStrings.titleApp, style: AppTextStyles.textTitle, textAlign: TextAlign.center, maxLines: 1),
+      ],
+    );
 }
 
-class ActionButton extends StatelessWidget {
-  const ActionButton({required this.icon, required this.onPressed, super.key});
-
-  final IconData icon;
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) =>
-      IconButton(icon: Icon(icon, size: 24), onPressed: onPressed);
-}
-
-class ActionButtonRow extends StatelessWidget {
-  const ActionButtonRow({required this.children, super.key});
-
-  final List<Widget> children;
-
-  @override
-  Widget build(BuildContext context) =>
-      Row(mainAxisAlignment: MainAxisAlignment.end, children: children);
-}
-
-class NavigationTabItem extends StatelessWidget {
-  const NavigationTabItem({required this.icon, required this.label, super.key});
-
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) => Column(
-    children: <Widget>[
-      // アイコンとテキスト
-      Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(icon, size: 24),
-          const SizedBox(height: 4),
-          Text(label, style: AppTextStyles.textSm),
-        ],
-      ),
-      // 下線
-      Container(height: 2, width: 24, color: Theme.of(context).colorScheme.primary),
-    ],
-  );
-}
-
-class NavigationTabBar extends StatelessWidget {
-  const NavigationTabBar({required this.items, super.key});
-
-  final List<NavigationTabItem> items;
-
-  @override
-  Widget build(BuildContext context) => Row(
-    mainAxisSize: MainAxisSize.min,
-    children: items.map((NavigationTabItem item) => Expanded(child: item)).toList(),
-  );
-}
