1537b0b6626a808d9d1054936b23c84762ae76da
penne <penne0505pp@gmail.com>
2025-09-25 22:36:41 +0900

    feat(app): add inventory route to AppRouter for navigation

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:go_router/go_router.dart";
 
+import "../../features/inventory/presentation/pages/inventory_management_page.dart";
 import "../../features/order/presentation/pages/order_history_page.dart";
 import "../../features/order/presentation/pages/order_management_page.dart";
 
@@ -20,6 +21,11 @@ class AppRouter {
         path: "/history",
         name: "history",
         builder: (BuildContext context, GoRouterState state) => const OrderHistoryPage(),
+      ),
+      GoRoute(
+        path: "/inventory",
+        name: "inventory",
+        builder: (BuildContext context, GoRouterState state) => const InventoryManagementPage(),
       ),
     ],
     // TODO: 認証ガードやリダイレクトはここに設定
